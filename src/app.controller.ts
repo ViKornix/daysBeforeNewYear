54007d5cb9027abf587cc1987f629335b926fa6f
Viktor <kitkat_115@mail.ru>
2026-02-28 14:20:27 +0300

    update: add getDays route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getDays() {
+    return this.appService.getDays();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getDays() {
+    const now = new Date()
+    const newYear = new Date(now.getFullYear(), 11, 31)
+    const diff = newYear.getTime() - now.getTime()
+    const days = Math.ceil(diff / (1000 * 60 * 60 * 24))
+
+    return {
+      data: {
+        daysBeforeNewYear: days,
+        now: now.toLocaleString()
+      }
+    }
   }
 }
